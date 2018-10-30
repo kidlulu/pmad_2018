683a08c64e5115091b3caec5a9efb2607701f018
kidlulu <luiz.rubens@gmail.com>
2018-10-30 16:14:54 -0300

    Atualização da programação do R

--- script.R
+++ script.R
@@ -1,7 +1,20 @@
 library(RODBC)
+library(dplyr)
 rm(list = ls())
 db <- RODBC::odbcConnect("db_codeplan2", uid=35866, pwd='Meliodas083')
-RODBC::sqlTables(db)
-tabelas <- RODBC::sqlTables(db)
-tabelas[tabelas[,2]=='pmad2018',]$TABLE_NAME
-visita <- RODBC::sqlQuery(db,"select * from pmad2018.visita_1023")
+RODBC::sqlTables(db) %>% 
+  dplyr::filter(TABLE_SCHEM=="pmad2018") %>%
+  dplyr::select(TABLE_NAME)
+
+v <- RODBC::sqlQuery(db,"select * from pmad2018.visita_1023")
+
+colnames(visita)
+
+
+
+table(visita$municipio)
+
+visita$municipio <- case_when(visita$municipio=="Águas Lindas"~"5200258")
+
+visita$A01municipio <- stringr::str_replace(visita$A01municipio, "Águas Lindas", "5200258")
+
